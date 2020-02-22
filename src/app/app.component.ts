2b6193f6200828a9ab755c733fdcd608dfd1f36b
Chethan <chethan477@hotmail.com>
2020-02-22 07:16:28 +0530

    add: call a method to set current user id
    * Set the current user id
    * Pass the current user id as attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat-app';
+  myUID = 0;
+
+  constructor(
+    private _msgService: MessageService
+  ) { }
+
+  ngOnInit() {
+    this._msgService.setMyUID(this.myUID);
+  }
 }
